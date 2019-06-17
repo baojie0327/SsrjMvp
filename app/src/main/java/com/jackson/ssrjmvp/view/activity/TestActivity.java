26934feba73ee198185d2d660f5fd11cfa012f94
黄宝杰 <huangbaojie0327@gmail.com>
2019-06-17 14:42:49 +0800

    no message

---
+++ app/src/main/java/com/jackson/ssrjmvp/view/activity/TestActivity.java
@@ -0,0 +1,15 @@
+package com.jackson.ssrjmvp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jackson.ssrjmvp.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
