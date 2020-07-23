70053804ba262b815a71a2604be2dd61dc4845e3
Jackson <bj0327_work@163.com>
2020-07-23 15:23:27 +0800

    no message

---
+++ app/src/main/java/com/jackson/ssrjmvp/svg/GlideToVectorYou.java
@@ -0,0 +1,98 @@
+package com.jackson.ssrjmvp.svg;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ImageView;
+
+
+
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+
+
+public class GlideToVectorYou {
+
+    private static GlideToVectorYou instance;
+
+    private Activity activity;
+    private RequestBuilder<PictureDrawable> requestBuilder;
+    private int placeHolderLoading = -1;
+    private int placeHolderError = -1;
+
+    public static GlideToVectorYou init(){
+        if (instance == null) {
+            instance = new GlideToVectorYou();
+        }
+
+        return instance;
+    }
+
+    public GlideToVectorYou with(Activity activity){
+        this.activity = activity;
+        createRequestBuilder();
+
+        return instance;
+    }
+
+    public GlideToVectorYou withListener(GlideToVectorYouListener listener) {
+        requestBuilder.listener(new SvgSoftwareLayerSetter(listener));
+
+        return instance;
+    }
+
+    public GlideToVectorYou setPlaceHolder(int placeHolderLoading, int placeHolderError) {
+        this.placeHolderError = placeHolderError;
+        this.placeHolderLoading = placeHolderLoading;
+
+        return instance;
+    }
+
+    public void load(Uri uri, ImageView imageView) {
+        if (placeHolderLoading != -1 && placeHolderError != -1) {
+            requestBuilder.apply(
+                    new RequestOptions()
+                            .placeholder(placeHolderLoading)
+                            .error(placeHolderError));
+        }
+
+        requestBuilder.load(uri).into(imageView);
+
+    }
+
+
+
+    public static void justLoadImage(Activity activity, Uri uri, ImageView imageView){
+        GlideApp.with(activity)
+                .as(PictureDrawable.class)
+                .listener(new SvgSoftwareLayerSetter()).load(uri).into(imageView);
+    }
+
+    public static void justLoadImageAsBackground(Activity activity, Uri uri, final View view) {
+        GlideApp.with(activity).load(uri).into(new SimpleTarget<Drawable>() {
+            @Override
+            public void onResourceReady(@NonNull Drawable resource, Transition<? super Drawable> transition) {
+                view.setBackground(resource);
+            }
+        });
+    }
+
+    public RequestBuilder<PictureDrawable> getRequestBuilder() {
+        return requestBuilder;
+    }
+
+    private void createRequestBuilder(){
+        requestBuilder = GlideApp.with(activity)
+                .as(PictureDrawable.class)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .listener(new SvgSoftwareLayerSetter());
+    }
+
+}
---
+++ app/src/main/java/com/jackson/ssrjmvp/svg/GlideToVectorYouListener.java
@@ -0,0 +1,6 @@
+package com.jackson.ssrjmvp.svg;
+
+public interface GlideToVectorYouListener {
+    void onLoadFailed();
+    void onResourceReady();
+}
